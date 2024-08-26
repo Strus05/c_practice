a0950b073d0fb286608bf8f93de43d8849d0dcac
Strus5 <79532476561r@gmail.com>
2024-08-27 02:33:25 +0300

    Исправить Операции присваивания, Добавить недостающие исп. файлы

--- Операции присваивания/case1.c
+++ Операции присваивания/case1.c
@@ -17,7 +17,7 @@ int main(){
     var2 ^= var1;
     var1 ^= var2;
     printf("var1 = %d, var2 = %d\n", var1, var2);
-    var1 -= var2 = -var2 + var1 += var2;
+    var1 -= var2 = -var2 + (var1 += var2);
     printf("var1 = %d, var2 = %d\n", var1, var2);
     return 0;
 }
--- Поразрядные операции/case1.c
+++ Поразрядные операции/case1.c
@@ -3,6 +3,6 @@
 int main(){
     int num1 = 0b01011;
     int num2 = num1 ^ 0b0110;
-    printf("%d при  смене 1 и 2 бита превращается в %d", num1, num2);
+    printf("number %d when bits 1 and 2 are swapped = %d", num1, num2);
     return 1;
 }
